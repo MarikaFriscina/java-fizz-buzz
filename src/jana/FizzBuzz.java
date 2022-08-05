54b3105ff98cf04443f921e5ff80d33377052c6b
MarikaFriscina <friscinamarika@gmail.com>
2022-08-05 12:09:24 +0200

    esercizio bonus completato

--- src/jana/FizzBuzz.java
+++ src/jana/FizzBuzz.java
@@ -1,19 +1,38 @@
 package jana;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		for(int i=0;i<101;i++) {
-						
-			if(i%3==0 ) {
-				System.out.println("Fizz");
-			} else if(i%5==0) {
-				System.out.println("FizzBuzz");
-			}else {
-				System.out.println(i);
+		
+		Scanner scan= new Scanner(System.in);
+		
+		System.out.println("Da 2 a 200, dimmi fino a che numero vuoi giocare: ");
+		int numero=scan.nextInt();
+		
+		if(numero<2 || numero>200) {
+			
+			System.out.println("Il numero inserito non è valido");				
+		
+		} else {
+		
+			for(int i=2;i<=numero;i++) {
+									
+				if(i%3==0 ) {
+					System.out.println("Fizz");
+				} else if(i%5==0) {
+					System.out.println("FizzBuzz");
+				}else {
+					System.out.println(i);
+				}
 			}
+		}
+						
+					
+			scan.close();
 			
 		}
 	}
 
-}
+
