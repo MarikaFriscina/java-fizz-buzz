9dcfa295517103251ec3f62fa76c67d5bcda47f9
MarikaFriscina <friscinamarika@gmail.com>
2022-08-05 11:53:52 +0200

    esercizio completato

--- src/jana/FizzBuzz.java
+++ src/jana/FizzBuzz.java
@@ -4,7 +4,15 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		for(int i=0;i<101;i++) {
-			System.out.println(i);
+						
+			if(i%3==0 ) {
+				System.out.println("Fizz");
+			} else if(i%5==0) {
+				System.out.println("FizzBuzz");
+			}else {
+				System.out.println(i);
+			}
+			
 		}
 	}
 
